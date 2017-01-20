ad11173309524b152ddfb1ff7be6fb8996afc45f
root <root@wewlads.club>
2017-01-20 09:43:03 -0500

    Remove server specific settings.
    To use include a module named 'lads' in the modulepath and add users/settings there.

--- manifests/base.pp
+++ manifests/base.pp
@@ -6,11 +6,6 @@ class wewlads::base (
 
   include wewlads::webserver
 
-  wewlads::user { 'demophoon':
-    ssh_key      => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCoGprqMrYegwmvsbyENxYThKBOO5Ep8FUnD99Ms244njxdoGp3AbvQZSytL0wkzhcNoQoGe++Z1AWet0NpudwujF0EphZoffWt+0dRBlxeGBfdRydQMngBA6V95NBvugBUyxH9p9ehC/tz6+tKP4iCfPhJAyuBjDKfSiIuZoSRraH+83nlK/Tqgh3KrLlYVRblCl9XxWLIq3UCpoeSl9jQPRXda9u2BUsVu821GfdSFiC2tKpQYSXOJcVZpPI/Zuv2B589FER0ceEuwbOHy/8LHrJEE67tW6W6mb7vBKIp7eTq61ts3jNRMHB3LVBgnS3YzkAA9BlecPvqQlTsHyRH',
-    staff        => true,
-  }
-
   cron { 'puppet apply':
     command => "/opt/puppetlabs/puppet/bin/puppet apply --modulepath ${wewlads::params::apply_module_path} ${wewlads::params::site_pp}",
     user    => 'root',
