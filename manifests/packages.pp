1bbec462129e225006da471ec568b8f6e98bf483
root <root@wewlads.club>
2017-01-21 12:52:30 -0500

    Add epel to installed packages on redhat

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,12 +1,12 @@
 class wewlads::packages {
-  $install_packages = [
+  $install_packages = concat([
     'vim',
     'emacs',
     'irssi',
     'bash',
     'ntp',
     'zsh',
-  ]
+  ], $wewlads::params::base_install_packages)
 
   package { $install_packages:
     ensure => present,
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,4 +1,13 @@
 class wewlads::params {
   $apply_module_path = '/root/modules'
   $site_pp = "${apply_module_path}/wewlads/site.pp"
+
+  case $::operatingsystem {
+    'redhat': {
+      $base_install_packages = ['epel-release']
+    }
+    default: {
+      $base_install_packages = []
+    }
+  }
 }
